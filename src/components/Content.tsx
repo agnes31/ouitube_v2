3a967aa032f0f473274723756c63d9d80cb6d5b7
agnes31 <agnesomet@gmail.com>
2024-11-22 14:17:33 +0700

    Utilisation de state dans React et Manipulation du DOM après affichage

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 export default function Content() {
     
     const formations : string[] = [
@@ -6,8 +8,32 @@ export default function Content() {
         "Formation Node.js",    
     ];
 
-    const isDisplay: boolean = true;
+    // const isDisplay: boolean = true;
     const age: number = 25
+    // let clickCount: number = 0
+    const [clickCount, setClickCount] = useState<number>(0)
+
+    const handleMoins = () => {
+        const moinsClickCount = clickCount - 1 
+        setClickCount(moinsClickCount);
+        
+    }
+    const handlePlus = () => {
+        const plusClickCount = clickCount + 1 
+        setClickCount(plusClickCount);
+        
+    }
+
+    useEffect(() => {
+    
+    document.title = `Vous avez cliqué ${clickCount} fois`;
+    
+    //     const content = document.querySelector('.content');
+    // console.log(content);
+    // setClickCount(clickCount + 1)
+
+    // },[]);
+    });
 
     return (
         <div className='content'> 
@@ -20,7 +46,10 @@ export default function Content() {
                     })
                 }
             </ul>
-            {isDisplay ? <button>Display</button> : null}
+            {/* {isDisplay ? <button>Display</button> : null} */}
+            <button onClick = {handleMoins}>- </button>
+            <p>Vous avez cliqué {clickCount}</p>
+            <button onClick = {handlePlus}>+ </button>
         </div>
     );
 }
