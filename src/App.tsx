c65509904ade6478bc009db05abccf6d39fcd123
agnes31 <agnesomet@gmail.com>
2024-11-27 16:58:48 +0700

    Echange d'informations entre composants

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,29 @@ import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [footerClickCount, setFooterClickCount] = useState<number>(0);
   const title: string = 'Sabaidee Touny';
   const copyright: string = "All rights reserved";
+
+  const handleFooterClick = (data:any) => {
+    const newFooterClickCount = footerClickCount + 1;
+    console.log('Footer clicked :', newFooterClickCount);
+    setFooterClickCount(newFooterClickCount);
+    console.log({data});
+  }
+
   return (
     <div className="App">
       <Header />
       <Content />
-      <Footer title={title} copyright={copyright}/>
-      {/* <Footer title={title}/> */}
-      {/* <Footer /> */}
+      <Footer 
+      title={title} 
+      copyright={copyright} 
+      handleFooterClick={handleFooterClick}
+      />
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@
 
 
 // export default function Footer(props: Footprops): JSX.Element {
-  export default function Footer({ title, copyright }: { title: string; copyright?: string }) {
+  export default function Footer({ title, copyright, handleFooterClick}: { title: string; copyright? : string; handleFooterClick: (data:any) => void; }) {
   console.log({ title, copyright });
 
     const style: React.CSSProperties = {
@@ -21,7 +21,8 @@
 
     // const CopyRight: string = "All rights reserved";
     return (
-      <footer style={style}>
+      // <footer style={style} onClick={handleFooterClick}>
+      <footer style={style} onClick={() => handleFooterClick({title, copyright})}>
           <h3>{title}</h3>
           {copyright && <p>{copyright}</p>}
       </footer>
